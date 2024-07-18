b830382934ee2bb0f36d26ea022b4f6faff52fce
Emil Andersen <emiand@norlys.dk>
2024-07-18 12:35:35 +0200

    move sorting options to the left

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -35,25 +35,25 @@ const Products: React.FC<PageProps> = ({ products, page, sort, onDelete }) => {
                 <div className="inline-flex items-center px-3 py-1 border border-gray-300 rounded-md bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                     <div className="relative inline-block text-left">
 
-                    <div>
-                        <button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900" id="menu-button" aria-expanded={isOpen} aria-haspopup="true" onClick={() => setIsOpen(!isOpen)}>
-                            {sort ? sort : "Sort"}
-                            <svg className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                    </div>
-
-                    {isOpen && (
-                        <div className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
-                            <div className="py-1" role="none">
-                                <Link href={`/products?page=${page}&sort=A-Z`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">A-Z</Link>
-                                <Link href={`/products?page=${page}&sort=Z-A`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">Z-A</Link>
-                                <Link href={`/products?page=${page}&sort=Newest`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">Newest</Link>
-                                <Link href={`/products?page=${page}&sort=Oldest`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">Oldest</Link>
-                            </div>
+                        <div>
+                            <button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900" id="menu-button" aria-expanded={isOpen} aria-haspopup="true" onClick={() => setIsOpen(!isOpen)}>
+                                {sort ? sort : "Sort"}
+                                <svg className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
                         </div>
-                    )}
+
+                        {isOpen && (
+                            <div className="absolute left-0 z-10 mt-2 w-40 origin-top-left rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
+                                <div className="py-1" role="none">
+                                    <Link href={`/products?page=${page}&sort=A-Z`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">A-Z</Link>
+                                    <Link href={`/products?page=${page}&sort=Z-A`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">Z-A</Link>
+                                    <Link href={`/products?page=${page}&sort=Newest`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">Newest</Link>
+                                    <Link href={`/products?page=${page}&sort=Oldest`} onClick={() => setIsOpen(false)} className="block px-4 py-2 text-sm text-gray-500" role="menuitem">Oldest</Link>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
 
